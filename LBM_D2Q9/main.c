024e4d10777a9124d18a59040619f227bc8c18cb
Juvenal Leon <jleon@cicese.edu.mx>
2016-02-09 16:41:51 -0800

    Initial Commit

---
+++ LBM_D2Q9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LBM_D2Q9
+//
+//  Created by Juvenal on 2/9/16.
+//  Copyright © 2016 Juvenal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
